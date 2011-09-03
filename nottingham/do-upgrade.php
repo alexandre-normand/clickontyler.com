77d9bd21088b643b54d064a60cbc13b5c86dfc64
Tyler Hall <tylerhall@gmail.com>
2011-09-03 09:19:59 +0000

    Adding serial number lookup and upgrades info to contact page. Adding templating to N2 upgrade page.

--- nottingham/do-upgrade.php
+++ nottingham/do-upgrade.php
@@ -1,3 +1,8 @@
+---
+title: Nottingham 2.0 Upgrade
+layout: default
+permalink: /do-upgrade.php
+---
 <?PHP
 	require '/var/www/shine.clickontyler.com/includes/master.inc.php';
 
@@ -5,45 +10,63 @@
 	$email = $db->escape($_POST['email']);
 	$row = $db->getRow("SELECT * FROM shine_orders WHERE app_id = '4' AND payer_email = '$email' ORDER BY dt DESC");
 	
+	$out = '';
+	
 	if($row === false)
 	{
-		echo "We're sorry, but we couldn't find an order with that email address. Feel free to contact us at support@clickontyler.com and we'll be happy to help find your order.";
-		exit;
+		$out .= "<p>We're sorry, but we couldn't find an order with that email address. Feel free to contact us at <a href='mailto:support@clickontyler.com'>support@clickontyler.com</a> and we'll be happy to help find your order.</p>";
 	}
-
-	if($row['dt'] > '2011-01-02 00:00:00')
+	else
 	{
-		if($row['upgrade_coupon'] == '')
+		if($row['dt'] > '2011-01-02 00:00:00')
 		{
-			echo "Our records show that you qualify for a free upgrade to Nottingham 2.0, however it looks like you've already claimed your new license. If you think this is a mistake, or just need help looking up a lost license, feel free to contact us at support@clickontyler.com.";
-			exit;
+			if($row['upgrade_coupon'] == '')
+			{
+				$out .= "<p>Our records show that you qualify for a free upgrade to Nottingham 2.0, however it looks like you've already claimed your new license. If you think this is a mistake, or just need help looking up a lost license, feel free to contact us at <a href='mailto:support@clickontyler.com'>support@clickontyler.com</a>.</p>";
+			}
+			else
+			{
+			    $app = new Application(11);
+
+				$o = new Order();
+				$o->first_name  = $row['first_name'];
+				$o->last_name   = $row['last_name'];
+				$o->payer_email = $row['payer_email'];
+				$o->app_id      = $app->id;
+				$o->type        = 'Upgrade';
+				$o->dt          = dater();
+				$o->item_name   = 'Nottingham 2.0';
+				$o->notes       = 'Automated Nottingham 2.0 upgrade';
+				$o->insert();
+
+				$o->generateLicense();
+				$o->emailLicense();
+
+				$out .= "<p>Yay! You qualified for a free upgrade to Nottingham 2.0. We have emailed an upgraded license key to you at " . $row['payer_email'] . ". Enjoy!</p>";
+
+				$db->query("UPDATE shine_orders SET upgrade_coupon = '' WHERE app_id = '4' AND payer_email = '$email'");
+			}
 		}
-		
-	    $app = new Application(11);
 
-		$o = new Order();
-		$o->first_name  = $row['first_name'];
-		$o->last_name   = $row['last_name'];
-		$o->payer_email = $row['payer_email'];
-		$o->app_id      = $app->id;
-		$o->type        = 'Upgrade';
-		$o->dt          = dater();
-		$o->item_name   = 'Nottingham 2.0';
-		$o->notes       = 'Automated Nottingham 2.0 upgrade';
-		$o->insert();
-
-		$o->generateLicense();
-		$o->emailLicense();
-
-		echo "Yay! You qualified for a free upgrade to Nottingham 2.0. We have emailed an upgraded license key to you at " . $row['payer_email'] . ". Enjoy!";
-
-		$db->query("UPDATE shine_orders SET upgrade_coupon = '' WHERE app_id = '4' AND payer_email = '$email'");
-
-		exit;
+		if(strlen($row['upgrade_coupon']) > 0)
+		{
+			$url = "https://sites.fastspring.com/clickontyler/instant/nottingham?coupon=" . $row['upgrade_coupon'];
+			redirect($url);
+		}
+		else
+		{
+			$out .= "<p>Our records show that you have already upgraded to Nottingham 2.0. If you think this is a mistake, or just need help <a href='/support/'>looking up a lost license</a>, feel free to contact us at <a href='mailto:support@clickontyler.com'>support@clickontyler.com</a>.</p>";
+		}
 	}
-
-	if(strlen($row['upgrade_coupon']) > 0)
-	{
-		$url = "https://sites.fastspring.com/clickontyler/instant/nottingham?coupon=" . $row['upgrade_coupon'];
-		redirect($url);
-	}
+?>
+<div class='contact' id='bd'> 
+	<div class='yui-gd band1'> 
+		<div class='yui-u first'> 
+			<img src='{{ site.cdn_url }}/images/hats.jpg' /> 
+		</div> 
+		<div class='yui-u'>
+			<h2>Nottingham 2.0 Upgrade</h2>
+			<?PHP echo $out; ?>
+		</div> 
+	</div> 
+</div>
---
+++ support/serial.php
@@ -0,0 +1,43 @@
+---
+title: Click On Tyler Support
+layout: default
+permalink: /serial.php
+---
+<?PHP
+	require '/var/www/shine.clickontyler.com/includes/master.inc.php';
+
+	$out = '';
+
+	$db = Database::getDatabase();
+	$email = $db->escape($_POST['email']);
+	$orders = DBObject::glob('Order', "SELECT * FROM shine_orders WHERE payer_email = '$email'");
+	if(count($orders) == 0) {
+		$out .= "<h2>Oops!</h2>";
+		$out .= "<p>We searched all of our records but couldn't find an order with that email address? Are you sure you entered the correct address? ";
+		$out .= "Feel free to email <a href='mailto:support@clickontyler.com'>support@clickontyler.com</a> and we'll be happy to help find your order manually.</p>";
+	} else {
+		$out .= "<h2>Yay!</h2>";
+		$out .= "<p>We found your orders listed below and have re-sent your registration email. If you don't see it in the next few minutes, please check your spam folder &mdash; ";
+		$out .= "it will be coming from <a href='mailto:support@clickontyler.com'>support@clickontyler.com</a>. If you no longer have access to that address or have any other questions ";
+		$out .= "please let us know and we'll be happy to help.</p>";
+		$out .= "<ul>";
+	
+		foreach($orders as $o) {
+			$o->emailLicense();
+			$a = new Application($o->app_id);
+			$out .= "<li>" . $a->name . "</li>";
+		}
+
+		$out .= "</ul>";
+	}
+?>
+<div class='contact' id='bd'> 
+	<div class='yui-gd band1'> 
+		<div class='yui-u first'> 
+			<img src='{{ site.cdn_url }}/images/hats.jpg' /> 
+		</div> 
+		<div class='yui-u'> 
+			<?PHP echo $out; ?>
+		</div> 
+	</div> 
+</div>
