ff240a39c049e7f2fbb4e6be82bd91147fd28863
Tyler Hall <tylerhall@gmail.com>
2012-02-06 19:41:13 +0000

    Updated GitHub summary to sort by number of watchers

--- github/index.php
+++ github/index.php
@@ -32,19 +32,38 @@
 	$url = "http://github.com/api/v2/xml/repos/show/$username";
 	$xmlstr = geturl($url);
 	$xml = simplexml_load_string($xmlstr);
+	
+	$data = array();
+	foreach($xml->repository as $repo)
+	{
+		$tmp             = array();
+		$tmp['name']     = (string)$repo->name;
+		$tmp['watchers'] = (string)$repo->watchers;
+		$tmp['forks']    = (string)$repo->forks;
+		$data[]          = $tmp;
+	}
+
+	usort($data, 'cmp');
+	function cmp($a, $b)
+	{
+		if($a['watchers'] == $b['watchers']) return 0;
+		return $a['watchers'] < $b['watchers'] ? 1 : -1;
+	}
 ?>
-<h1>GitHub Summary</h1>
+<h1>GitHub Summary for <a href="http://github.com/<?PHP echo $username; ?>"><?PHP echo $username; ?></a></h1>
+<p>Want a GitHub summary page like this one for yourself? The source is <a href="https://github.com/tylerhall/clickontyler.com/blob/master/github/index.php">available is here</a>.</p>
+<p><em>Repositories are ordered by number of people watching.</em></p>
 <table>
 	<tr>
 		<th>Repository</th>
 		<th>Watchers</th>
 		<th>Forks</th>
 	</tr>
-	<?PHP foreach($xml->repository as $repo) : ?>
+	<?PHP foreach($data as $repo) : ?>
 	<tr>
-		<td><a href="http://github.com/<?PHP echo $username; ?>/<?PHP echo (string)$repo->name; ?>/"><?PHP echo (string)$repo->name; ?></a></td>
-		<td><a href="http://github.com/<?PHP echo $username; ?>/<?PHP echo (string)$repo->name; ?>/watchers"><?PHP echo (string)$repo->watchers; ?></a></td>
-		<td><a href="http://github.com/<?PHP echo $username; ?>/<?PHP echo (string)$repo->name; ?>/network"><?PHP echo (string)$repo->forks; ?></a></td>
+		<td><a href="http://github.com/<?PHP echo $username; ?>/<?PHP echo $repo['name']; ?>/"><?PHP echo $repo['name']; ?></a></td>
+		<td><a href="http://github.com/<?PHP echo $username; ?>/<?PHP echo $repo['name']; ?>/watchers"><?PHP echo $repo['watchers']; ?></a></td>
+		<td><a href="http://github.com/<?PHP echo $username; ?>/<?PHP echo $repo['name']; ?>/network"><?PHP echo $repo['forks']; ?></a></td>
 	</tr>
 	<?PHP endforeach; ?>
 </table>
