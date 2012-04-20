45eca8063f20413c457a7776792cf92e75e93c07
Tyler Hall <tylerhall@gmail.com>
2012-04-20 18:46:10 +0000

    Fixed VHX Account page that wasn't on the new design.

--- virtualhostx/account/index.php
+++ virtualhostx/account/index.php
@@ -1,6 +1,6 @@
 ---
 title: VirtualHostX - Your Account
-layout: default
+layout: page
 permalink: /index.php
 ---
 <?PHP
@@ -27,59 +27,40 @@ permalink: /index.php
 	$orders  = DBObject::glob('Order', "SELECT * FROM shine_orders WHERE payer_email = $_email ORDER BY dt DESC");
 	$tunnels = DBObject::glob('VHXTunnel', "SELECT * FROM vhx_tunnels WHERE user_id = '{$VHXAuth->id}' ORDER BY dt_created DESC LIMIT 10");
 ?>
-<div id="bd" class="product">
-	<div class="yui-gd band1"> 
-	    <div class="yui-u first center"> 
-			<img src="{{ site.cdn_url }}/images/virtualhostx-logo180.png" width="180" height="180"> 
-	    </div> 
-	    <div class="yui-u left">
-			<h2>Your Account</h2> 
-			<p><em>Manage your Lift Off account</em></p>
-			<p>Thanks for using Lift Off. From this page you can purchase a subscription or pay-as-you-go credits and view recent transactions<!-- , and change your account password -->.</p>
-			<p>If you have any questions or need further help, feel free to contact us at <a href="mailto:support@clickontyler.com">support@clickontyler.com</a></p>
-	    </div> 
-	</div> 
-	<div class="yui-g band4">
-		<h3>Account Info</h3>
-		<form action="/virtualhostx/account" method="post">
-			<p>You are logged in as <em><?PHP echo $VHXAuth->username; ?></em> and have <em><?PHP echo $account_status; ?></em>.</p><br>
-			<!-- <p>New Password: <input type="text" name="password" value="" id="password"> <input type="submit" name="btnPassword" value="Change Password" id="btnPassword"></p> -->
-		</form>
-	</div> 
-	<div class="yui-g band3">
-		<h3>Purchase Credits</h3>
-		<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">5 Credits</a></em></p>
-		<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">10 Credits</a></em> - Save 10%</p>
-		<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">25 Credits</a></em> - Save 20%</p>
-		<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">Monthly Subscription</a></em> - a monthly subscription to Lift Off lets you use the service as often as you want.</p>
-	</div> 
-	<div class="yui-g band4">
-		<div class="yui-u first">
-			<h3>Purchase History</h3>
-			<ul>
-				<?PHP foreach($orders as $o) : ?>
-				<?PHP if($o->app_id == 18) : ?>
-				<li><?PHP echo dater($o->dt, 'F j, Y'); ?> - <a href="<?PHP echo $o->subscriptionURL(); ?>"><?PHP echo $o->applicationName(); ?></a></li>
-				<?PHP else : ?>
-				<li><?PHP echo dater($o->dt, 'F j, Y'); ?> - <?PHP echo $o->applicationName(); ?></li>
-				<?PHP endif; ?>
-				<?PHP endforeach; ?>
-			</ul>
-		</div>
+	<h2>Your Account</h2> 
+	<p><em>Manage your Lift Off account</em></p>
+	<p>Thanks for using Lift Off. From this page you can purchase a subscription or pay-as-you-go credits and view recent transactions<!-- , and change your account password -->.</p>
+	<p>If you have any questions or need further help, feel free to contact us at <a href="mailto:support@clickontyler.com">support@clickontyler.com</a></p>
 
-		<div class="yui-u">
-			<h3>Recent Lift Off Usage</h3>
-			<table style="width:100%;">
-				<?PHP foreach($tunnels as $t) : ?>
-				<tr>
-					<td><?PHP echo dater($t->dt_created, 'Y-m-d g:ia'); ?></td>
-					<td><?PHP echo $t->hostname; ?></td>
-				</tr>
-				<?PHP endforeach; ?>
-			</table>
-		</div>
-    </div> 
-	<div class="yui-g band5">
-		
-	</div>
-</div>
+	<h3>Account Info</h3>
+	<form action="/virtualhostx/account" method="post">
+		<p>You are logged in as <em><?PHP echo $VHXAuth->username; ?></em> and have <em><?PHP echo $account_status; ?></em>.</p><br>
+		<!-- <p>New Password: <input type="text" name="password" value="" id="password"> <input type="submit" name="btnPassword" value="Change Password" id="btnPassword"></p> -->
+	</form>
+
+	<h3>Purchase Credits</h3>
+	<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">5 Credits</a></em></p>
+	<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">10 Credits</a></em> - Save 10%</p>
+	<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">25 Credits</a></em> - Save 20%</p>
+	<p><em><a href="https://sites.fastspring.com/clickontyler/instant/LiftOff">Monthly Subscription</a></em> - a monthly subscription to Lift Off lets you use the service as often as you want.</p>
+
+	<h3>Purchase History</h3>
+	<ul>
+		<?PHP foreach($orders as $o) : ?>
+		<?PHP if($o->app_id == 18) : ?>
+		<li><?PHP echo dater($o->dt, 'F j, Y'); ?> - <a href="<?PHP echo $o->subscriptionURL(); ?>"><?PHP echo $o->applicationName(); ?></a></li>
+		<?PHP else : ?>
+		<li><?PHP echo dater($o->dt, 'F j, Y'); ?> - <?PHP echo $o->applicationName(); ?></li>
+		<?PHP endif; ?>
+		<?PHP endforeach; ?>
+	</ul>
+
+	<h3>Recent Lift Off Usage</h3>
+	<table style="width:100%;">
+		<?PHP foreach($tunnels as $t) : ?>
+		<tr>
+			<td><?PHP echo dater($t->dt_created, 'Y-m-d g:ia'); ?></td>
+			<td><?PHP echo $t->hostname; ?></td>
+		</tr>
+		<?PHP endforeach; ?>
+	</table>
