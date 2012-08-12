dde8c052127af02d4e2b8010c930a52928c99d2d
Tyler Hall <tylerhall@gmail.com>
2012-08-12 03:46:37 +0000

    Add VHX sale copy

--- virtualhostx/purchase/index.php
+++ virtualhostx/purchase/index.php
@@ -1,3 +1,3 @@
 <?PHP
 	// header('Location: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=support%40clickontyler%2ecom&item_name=VirtualHostX&item_number=3&amount=19%2e00&no_shipping=1&no_note=1&currency_code=USD&lc=US&bn=PP%2dBuyNowBF&charset=UTF%2d8');
-    header('Location: https://sites.fastspring.com/clickontyler/instant/virtualhostx');
+    header('Location: https://sites.fastspring.com/clickontyler/instant/virtualhostx?coupon=FIVEYEAR');
