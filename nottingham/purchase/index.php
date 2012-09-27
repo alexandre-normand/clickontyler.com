2462827cee85a1b95b947d65089976aabda09f89
Tyler Hall <tylerhall@gmail.com>
2012-09-27 19:59:33 +0000

    Turning off Nottingham purhcases

--- nottingham/purchase/index.php
+++ nottingham/purchase/index.php
@@ -1,3 +1,4 @@
 <?PHP
     // header('Location: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=support%40clickontyler%2ecom&item_name=Nottingham&item_number=4&amount=14%2e00&no_shipping=1&no_note=1&currency_code=USD&lc=US&bn=PP%2dBuyNowBF&charset=UTF%2d8');
-    header('Location: https://sites.fastspring.com/clickontyler/instant/nottingham');
+    // header('Location: https://sites.fastspring.com/clickontyler/instant/nottingham');
+	header('Location: http://clickontyler.com/nottingham/');
